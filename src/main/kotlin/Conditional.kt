4c476e9c714c04020e84f5d14a1e3382310d844a
Mystlud <bayurahmatr@gmail.com>
2022-10-04 15:39:49 +0700

    commit 1

---
+++ src/main/kotlin/Conditional.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val isSamboGuilty = true
+    val isFalse = false
+    if (isSamboGuilty) {
+        println("He will be jailed")
+    } else if (isFalse) {
+        println("Something is wrong with this country")
+    } else {
+        println ("....")
+    }
+    
+    val num = 6
+    
+
+}
---
+++ src/main/kotlin/DataType.kt
@@ -0,0 +1,20 @@
+const val KONSTAN_DATA = "Data" //konstan berada di luar function
+
+fun main() {
+    val dataInt = 5
+    val dataString = "Halo Polban"
+    val dataBoolean = true
+
+    var dataIntVar = 10
+
+    //dataInt = 6
+    dataIntVar = 10
+
+
+
+//    println(dataInt.plus(other = 3))
+//    println(dataInt)
+//    println(dataIntVar)
+
+
+}
